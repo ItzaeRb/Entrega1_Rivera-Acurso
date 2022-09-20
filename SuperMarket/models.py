3ee64f87aaf570539160947c5c49617e439cc2b7
Itzae Rivera <itzae@ives.edu.mx>
2022-09-19 23:28:33 -0500

    Creación del primero modelo, para los productos

--- SuperMarket/models.py
+++ SuperMarket/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Productos(models.Model):
+    nombre = models.CharField(max_length=80)
+    clave = models.CharField(max_length=10)
+    categoria = models.CharField(max_length=80)
+    tipo = models.CharField(max_length=80)
+    marca = models.CharField(max_length=60)
+    unidadMedida = models.CharField(max_length=30)
+    fechaCaducidad = models.DateField()
+    stock = models.IntegerField()
