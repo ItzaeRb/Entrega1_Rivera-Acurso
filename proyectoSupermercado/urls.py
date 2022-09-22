f2e3e35b8a60e97d180417868a1bf2077610c533
Itzae Rivera <itzae@ives.edu.mx>
2022-09-22 18:24:46 -0500

    Se editaron las urls para que redirigan a la pagina principal de SuperMarket

--- proyectoSupermercado/urls.py
+++ proyectoSupermercado/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('SuperMarket/', include ("SuperMarket.urls"))
+    path('SuperMarket/', include ("SuperMarket.urls")),
+    path('', include ("SuperMarket.urls"))
 
 ]
