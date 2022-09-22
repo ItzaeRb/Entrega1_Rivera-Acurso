9d599bfd4b13fef1fe266d5ebfe5c3278899d91b
Itzae Rivera <itzae@ives.edu.mx>
2022-09-22 00:48:58 -0500

    Se editó la pantalla de inicio, y se añadió a las urls un path vacío que rediriga al home

--- SuperMarket/urls.py
+++ SuperMarket/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from SuperMarket.views import * 
 
 urlpatterns = [
+    path('', inicio),
     path('inicio/', inicio),
     path('create_productos/', create_productos),
     path('read_productos/', read_productos),
