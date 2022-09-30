8898088f00c3ed5c799007d63d4c8831c2f6c7e5
Jeremias Acurso <jeremias.acurso@hotmail.com>
2022-09-30 11:08:35 -0300

    se crea la clase userregisterform con nuevos inputs

--- SuperMarket/forms.py
+++ SuperMarket/forms.py
@@ -1,6 +1,13 @@
+from dataclasses import fields
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from platformdirs import user_cache_dir
 
 # se crean los formularios nuevos.
+
+
+
 class Form_clientes(forms.Form):
     claveCliente = forms.CharField()
     nombreProducto = forms.CharField()
@@ -10,3 +17,14 @@ class Form_clientes(forms.Form):
     fechaNacimiento = forms.DateTimeField()
     email = forms.EmailField()
     membresia = forms.CharField()
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    password1 = forms.CharField(label="Contraseña", widget= forms.PasswordInput)
+    password2 = forms.CharField(label="Repetir Contraseña", widget= forms.PasswordInput)
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+        help_text = {k:"" for k in fields}
+        
+    
